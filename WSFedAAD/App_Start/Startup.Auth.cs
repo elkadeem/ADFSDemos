4e6a16e45d8705f8fec3e3c7e539e146c6dfab10
Wael Elkadim <elkadeem@hotmail.com>
2022-03-14 12:26:38 +0200

    Add project files.

---
+++ WSFedAAD/App_Start/Startup.Auth.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSFedAAD
+{
+	public partial class Startup
+	{
+		public void ConfigAuth(IAppBuilder app)
+        {
+			app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+			app.UseCookieAuthentication(new CookieAuthenticationOptions()
+			{
+				CookieHttpOnly = true,
+			});
+
+			app.UseWsFederationAuthentication(new WsFederationAuthenticationOptions()
+			{
+				MetadataAddress = "https://adfs.contoso.com/federationmetadata/2007-06/federationmetadata.xml",
+				Wtrealm = "https://localhost:44391/"
+			});
+        }
+	}
+}
---
+++ WSFedAAD/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WSFedAAD
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ServicePointManager.ServerCertificateValidationCallback = delegate (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+                return true;
+            };
+        }
+    }
+}
